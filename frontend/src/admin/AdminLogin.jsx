303463d32484a7bed0ff6f599eb51c6dce9c4e1a
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2025-11-30 16:22:36 +0530

    Update AdminLogin.jsx

--- frontend/src/admin/AdminLogin.jsx
+++ frontend/src/admin/AdminLogin.jsx
@@ -8,6 +8,7 @@ function AdminLogin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  
   const [errorMessage, setErrorMessage] = useState("");
 
   const navigate = useNavigate();
