c6160ae977717a43b38c0125f9885315f60a3f3b
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2025-12-12 22:09:56 +0530

    Update admin.controller.js

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 import { z } from "zod";
 import config from "../config.js";
 import { Admin } from "../models/admin.model.js";
+
 export const signup = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
   
