06e9709fdc0056815c0b63eb7f24ea99cc17d7c0
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2026-01-19 14:31:03 +0530

    Update course.controller.js

--- backend/controllers/course.controller.js
+++ backend/controllers/course.controller.js
@@ -3,7 +3,6 @@ import { Purchase } from "../models/purchase.model.js";
 import { v2 as cloudinary } from 'cloudinary';
 
 
-
 export const createCourse = async (req, res) => {
   // const title=req.body.title;
   // const description=req.body.decription;
