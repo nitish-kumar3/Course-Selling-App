f8e8cbbc470644dccdb594d353a131bf1c1269e2
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2026-05-17 14:35:49 +0530

    Remove unnecessary blank line in admin.controller.js

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -5,7 +5,6 @@ import config from "../config.js";
 import { Admin } from "../models/admin.model.js";
 
 
-
 export const signup = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
   
