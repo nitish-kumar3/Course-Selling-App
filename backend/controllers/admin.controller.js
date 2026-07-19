72e75cf5ce4fcf51bac8f7e1bf572860a654f94d
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2026-07-19 23:49:11 +0530

    Update admin.controller.js

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -4,6 +4,8 @@ import { z } from "zod";
 import config from "../config.js";
 import { Admin } from "../models/admin.model.js";
 
+
+
 export const signup = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
   
